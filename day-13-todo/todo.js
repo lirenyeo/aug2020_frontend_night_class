f290125ec70e082ce39b88d509c6342b1b0ea328
Liren <liren90@gmail.com>
2020-10-01 22:30:35 +0800

    todo list updated with server communication

--- day-13-todo/todo.js
+++ day-13-todo/todo.js
@@ -3,12 +3,32 @@ const todoInput = document.querySelector('#todo-input')
 const addButton = document.querySelector('#add-btn')
 const list = document.querySelector('#todo-list')
 
+fetch('https://simple-crud-server.glitch.me/getTodos')
+  .then((resp) => resp.json())
+  .then((result) => {
+    // console.log(result)
+    result.forEach((item) => {
+      console.log(item)
+      const newItem = document.createElement('div')
+      newItem.classList.add('item')
+      newItem.innerText = item.todo
+      list.insertAdjacentElement('afterbegin', newItem)
+    })
+  })
+
 todoForm.onsubmit = (e) => {
   e.preventDefault()
   if (todoInput.value.length < 3) {
     return
   }
-  // put new todo item into <div id="todo-list"></div>
+
+  fetch('https://simple-crud-server.glitch.me/addTodo', {
+    method: 'POST',
+    body: JSON.stringify({ todo: todoInput.value }),
+    headers: { 'Content-Type': 'application/json' },
+  })
+
+  // dangerous way:
   // list.insertAdjacentHTML('afterbegin', `
   //   <div class="item">${todoInput.value}</div>
   // `)
@@ -28,11 +48,9 @@ todoForm.onsubmit = (e) => {
 todoInput.oninput = () => {
   if (todoInput.value.length >= 3) {
     addButton.disabled = false
-  } else [
-    addButton.disabled = true
-  ]
+  } else [(addButton.disabled = true)]
 }
 
-list.onclick = e => {
+list.onclick = (e) => {
   e.target.remove()
 }
